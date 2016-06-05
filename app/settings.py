9490706216fb18adb5ab1307e3170cd3e928dc7c
Jee Kang <jeekang908@gmail.com>
2016-06-04 18:24:17 -0700

    database

--- app/settings.py
+++ app/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# Uncomment for Heroku
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
 #Uncomment for local database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
-
-# Uncomment for Heroku
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config()
 }
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
